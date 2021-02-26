9220763620f6625f2b106256931ff3b32c9ba7f7
Luis1505 <luis.montanez-vazquez@patelhs.org>
2021-02-26 16:47:50 +0000

    I deleted the stupid file

--- Main.java
+++ Main.java
@@ -1,29 +1,29 @@
 class Main {
-  public static void main(String[] args) {
-    rocketLaunch(5);
-    endingLine(1);
+public static void main(String[] args) {
+    rocketLaunch(10);
+    landing(1);
   }
 // this is the rocket countdown that counts down until its ready to blast off 
-  public static void rocketLaunch(int n) {
-    if (n == 0) {
+public static void rocketLaunch(int x) {
+    if (x == 0) {
       System.out.println("Blast off");
-    } else {
-      System.out.println(n);
-      n--;
-      rocketLaunch(n);
+    }else{
+      System.out.println(x);
+      x--;
+      rocketLaunch(x);
    }
   } //This is the process of the rocket launching out of earth and to mars. 
-  public static void endingLine(int j){
-    if (j==0){
-      System.out.println("The lands at mars");
-    }else{
+public static void landing(int n){
+  if (n==0){
+      System.out.println("The rocket lands at mars");
+  }else{
       System.out.println("The rocket is leaves the mesosphere.");
       System.out.println("The rocket leaves earth and is in space.");
       System.out.println("The rocket comes across mars ");
       System.out.println("Will the rocket land perfectly on the martian surface?");
       System.out.println("The lands in Mars untouched!");
-      j--;
-      endingLine(j);
+      n--;
+      landing(n);
     }
   }
 }
